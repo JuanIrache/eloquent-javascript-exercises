81dd4e4e8693bb49e86b6bf55fdc5214af0aad62
JuanIrache <yrache@gmail.com>
2019-05-28 17:55:27 +0200

    complete exercise

--- 20_3_public_space/code.js
+++ 20_3_public_space/code.js
@@ -4,19 +4,30 @@ let filename = document.querySelector('#filename');
 let save = document.querySelector('#save');
 let del = document.querySelector('#delete');
 let root = 'http://localhost:8000';
-fetch(`${root}/`)
-  .then(res => {
-    if (res.status !== 200) console.error(`Error ${res.status}`);
-    else return res.text();
-  })
-  .then(text => {
-    const files = text.split('\n');
-    files.forEach(f => {
-      let option = document.createElement('option');
-      option.innerHTML = f;
-      select.appendChild(option);
+
+function loadSelect() {
+  fetch(`${root}/`)
+    .then(res => {
+      if (res.status !== 200) console.error(`Error ${res.status}`);
+      else return res.text();
+    })
+    .then(text => {
+      const files = text.split('\n');
+      select.innerHTML = '';
+      let choose = document.createElement('option');
+      choose.innerHTML = 'Choose file';
+      choose.value = '';
+      select.appendChild(choose);
+      files.forEach((f, i) => {
+        let option = document.createElement('option');
+        option.innerHTML = f;
+        select.appendChild(option);
+        if (f === filename.value) select.selectedIndex = i + 1;
+      });
     });
-  });
+}
+
+loadSelect();
 
 select.addEventListener('change', e => {
   if (select.value) {
@@ -36,13 +47,27 @@ select.addEventListener('change', e => {
 });
 
 save.addEventListener('click', e => {
+  e.preventDefault();
   if (filename.value) {
     fetch(`${root}/${filename.value}`, {
       method: 'PUT',
-      body: text.innerHTML,
+      body: text.value,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'text/plain'
       }
-    });
+    }).then(loadSelect);
+  } else console.error('missing filename');
+});
+
+del.addEventListener('click', e => {
+  if (filename.value) {
+    fetch(`${root}/${filename.value}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    }).then(loadSelect);
+    filename.value = '';
+    text.innerHTML = '';
   } else console.error('missing filename');
 });
